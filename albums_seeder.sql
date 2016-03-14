5d6de8c4b2e0d76a0b7878829fe27e861ef06d1d
CJ Sampson <cjsampson41@gmail.com>
2016-03-14 17:42:09 -0500

    condensed INSERT statments into a single statement

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,25 +1,44 @@
+-- USE codeup_test_db;
+
+-- INSERT INTO albums(artist, album_name, release_date, sales, genre)
+-- VALUES ('Michael Jackson', 'Thriller', 1982, 48.1, 'Pop'); 
+
+-- INSERT INTO albums(artist, album_name, release_date, sales, genre)
+-- VALUES ('The Eagles', 'Jagged Little Pill', 1995, 24.8, 'Alternative Rock');
+
+-- INSERT INTO albums(artist, album_name, release_date, sales, genre)
+-- VALUES ('Fleetwood Mac', 'Rumours', 1977, 32.2, 'Rock');
+
+-- INSERT INTO albums(artist, album_name, release_date, sales, genre)
+-- VALUES ('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'R&B');
+
+-- INSERT INTO albums(artist, album_name, release_date, sales, genre)
+-- VALUES ('AC/DC', 'Back in Black', 1980, 25.9, 'Hard Rock');
+
+-- INSERT INTO albums(artist, album_name, release_date, sales, genre)
+-- VALUES ('Pink Floyd', 'The Dark Side of the Moon', 1973, 22.7, 'Progressive Rock');
+
+-- INSERT INTO albums(artist, album_name, release_date, sales, genre)
+-- VALUES ('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard Rock');	   
+
+-- INSERT INTO albums(artist, album_name, release_date, sales, genre)
+-- VALUES ('Bee Gees', 'Saturday Night Fever', 1977, 19, 'Disco');	   
+
+
 USE codeup_test_db;
 
 INSERT INTO albums(artist, album_name, release_date, sales, genre)
-VALUES ('Michael Jackson', 'Thriller', 1982, 48.1, 'Pop'); 
+VALUES 
+	('Michael Jackson', 'Thriller', 1982, 48.1, 'Pop'),
+	('The Eagles', 'Jagged Little Pill', 1995, 24.8, 'Alternative Rock'),
+	('Fleetwood Mac', 'Rumours', 1977, 32.2, 'Rock'),
+	('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'R&B'),
+	('AC/DC', 'Back in Black', 1980, 25.9, 'Hard Rock'),
+	('Pink Floyd', 'The Dark Side of the Moon', 1973, 22.7, 'Progressive Rock'),
+	('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard Rock'),
+	('Bee Gees', 'Saturday Night Fever', 1977, 19, 'Disco');
 
-INSERT INTO albums(artist, album_name, release_date, sales, genre)
-VALUES ('The Eagles', 'Jagged Little Pill', 1995, 24.8, 'Alternative Rock');
 
-INSERT INTO albums(artist, album_name, release_date, sales, genre)
-VALUES ('Fleetwood Mac', 'Rumours', 1977, 32.2, 'Rock');
 
-INSERT INTO albums(artist, album_name, release_date, sales, genre)
-VALUES ('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'R&B');
 
-INSERT INTO albums(artist, album_name, release_date, sales, genre)
-VALUES ('AC/DC', 'Back in Black', 1980, 25.9, 'Hard Rock');
 
-INSERT INTO albums(artist, album_name, release_date, sales, genre)
-VALUES ('Pink Floyd', 'The Dark Side of the Moon', 1973, 22.7, 'Progressive Rock');
-
-INSERT INTO albums(artist, album_name, release_date, sales, genre)
-VALUES ('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard Rock');	   
-
-INSERT INTO albums(artist, album_name, release_date, sales, genre)
-VALUES ('Bee Gees', 'Saturday Night Fever', 1977, 19, 'Disco');	   
