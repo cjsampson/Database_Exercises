aea907f7b31f5a79fd6299924a0f6296ebe40770
CJ Sampson <cjsampson41@gmail.com>
2016-03-15 15:00:30 -0500

    BETWEEN operator statement

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,6 +5,15 @@ WHERE first_name
 IN ('Irena', 'Vidya', 'Maya');
 
 -- DESCRIBE employees;
+-- Return employees with a last name that starts with E
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%';
+
+-- DESCRIBE employees 
+-- Return employees hired in the 90's
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date 
+BETWEEN '1990-01-01' AND '1999-12-31';
+
