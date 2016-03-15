ba6241a11d050f7ffb0d4a148a7343e806d7983a
CJ Sampson <cjsampson41@gmail.com>
2016-03-15 15:54:44 -0500

    All employees whos lname starts with E and both st/end with E

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,37 +1,33 @@
--- DESCRIBE employees;
+-- Return fname 'Irena', 'Vidya', or 'Maya'
 SELECT first_name, last_name
 FROM employees
 WHERE first_name
 IN ('Irena', 'Vidya', 'Maya');
 
--- DESCRIBE employees;
 -- Return employees with a last name that starts with E
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%';
 
--- DESCRIBE employees 
 -- Return employees hired in the 90's
 SELECT first_name, last_name
 FROM employees
 WHERE hire_date 
 BETWEEN '1990-01-01' AND '1999-12-31';
 
--- DESCRIBE employees
 -- Return employees born on Christmas
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date 
 LIKE '%12-25';
 
--- DESCRIBE employees 
 -- Return employees with a 'q' in their last name
 SELECT first_name, last_name
 FROM employees
 WHERE last_name
 LIKE 'Q%';
 
--- DESCRIBE employees;
+
 -- Replace IN with OR
 SELECT first_name, last_name
 FROM employees
@@ -39,7 +35,6 @@ WHERE first_name =  'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya';
 
--- DESCRIBE employees;
 -- Add condition to find everybody with those names who is also male
 SELECT first_name, last_name, gender
 FROM employees
@@ -47,3 +42,14 @@ WHERE gender =  'M'
 AND  first_name IN (
 	'Maya', 'Irena', 'Vidya' );
 
+-- All employees whose last name starts or ends with 'E'
+SELECT first_name, last_name
+FROM employees 
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
+
+-- lname starts AND ends with E
+SELECT first_name, last_name
+FROM employees 
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
