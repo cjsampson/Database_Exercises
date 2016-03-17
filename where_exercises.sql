0e9aca97245af24fa5dfd8238365c5c58196981f
CJ Sampson <cjsampson41@gmail.com>
2016-03-17 11:43:19 -0500

    Where exercises cont'd

--- where_exercises.sql
+++ where_exercises.sql
@@ -53,3 +53,16 @@ SELECT first_name, last_name
 FROM employees 
 WHERE last_name LIKE 'E%'
 AND last_name LIKE '%E';
+
+-- hired in 90's and born on Xmas
+SELECT first_name, last_name, hire_date, birth_date
+FROM employees 
+WHERE birth_date LIKE '%12-25'
+AND hire_date LIKE '199%';
+
+-- Last Name has 'Q' but not 'QU'
+SELECT first_name, last_name
+FROM employees 
+WHERE last_name NOT LIKE '%QU%'
+AND last_name LIKE '%Q%';
+
