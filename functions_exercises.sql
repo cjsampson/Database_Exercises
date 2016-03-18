0ccc3e6abb43fe2a2587086bf1ff9be3d15794c6
CJ Sampson <cjsampson41@gmail.com>
2016-03-17 19:38:53 -0500

    CONCAT first_name, last_name

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,5 +1,4 @@
 -- Return number on males and number of females with first name credentials
-
 SELECT COUNT(gender), gender
 FROM employees
 WHERE first_name
@@ -13,3 +12,10 @@ WHERE first_name
 IN ('Irena', 'Vidya', 'Maya') 
 AND gender = 'f'
 ORDER BY last_name, first_name;
+
+-- CONCAT first_name, last_name
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees 
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E'
+ORDER BY emp_no;
