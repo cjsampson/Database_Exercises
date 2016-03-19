d9c576252c77cf9b6bd6c06f9b9f17f16e6dacfa
CJ Sampson <cjsampson41@gmail.com>
2016-03-19 13:44:31 -0500

    current manager fullname

--- joins.sql
+++ joins.sql
@@ -9,3 +9,10 @@ GROUP BY roles.name;
 -- employees DB
 -- each department 
 -- name of current manager of dept
+SELECT CONCAT(e.first_name, ' ', last_name) AS manager_full_name, d.dept_name
+FROM employees AS e
+JOIN dept_manager as dm
+  ON dm.emp_no = e.emp_no
+JOIN departments as d
+  ON d.dept_no = dm.dept_no
+WHERE to_date LIKE '9999%';
